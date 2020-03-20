3579fc1127b4c974e424df9bb323c463b3d608ea
Amin <31611764+amin19948@users.noreply.github.com>
2020-03-20 12:11:43 +0100

    create ticket controller & ticket model & setup frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'tickets#index'
+  get 'tickets/new'
+  get 'tickets/create'
+  get 'tickets/edit'
+  get 'tickets/update'
+  get 'tickets/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
